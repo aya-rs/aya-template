9e53687adfce489d059344352332d8aaf746655f
Alessandro Decina <alessandro.d@gmail.com>
2021-08-08 13:46:33 +0000

    Add simple port fw logic

--- {{project-name}}-ebpf/src/main.rs
+++ {{project-name}}-ebpf/src/main.rs
@@ -1,7 +1,80 @@
 #![no_std]
 #![no_main]
 
+use core::mem;
+use memoffset::offset_of;
+
+use aya_bpf::{
+    bindings::xdp_action::{XDP_DROP, XDP_PASS},
+    macros::{map, xdp},
+    maps::HashMap,
+    programs::XdpContext,
+};
+
+mod net;
+use net::{iphdr, tcphdr};
+
+use crate::net::ethhdr;
+
+#[map]
+static mut BLOCK_PORTS: HashMap<u16, u16> = HashMap::with_max_entries(1024, 0);
+
+#[xdp(name = "xdp_fw")]
+pub fn xdp_firewall(ctx: XdpContext) -> u32 {
+    match try_xdp_firewall(ctx) {
+        Ok(ret) => ret,
+        Err(_) => XDP_PASS,
+    }
+}
+
+fn try_xdp_firewall(ctx: XdpContext) -> Result<u32, ()> {
+    if let Some(port) = tcp_dest_port(&ctx)? {
+        if block_port(port) {
+            return Ok(XDP_DROP);
+        }
+    }
+
+    Ok(XDP_PASS)
+}
+
+fn tcp_dest_port(ctx: &XdpContext) -> Result<Option<u16>, ()> {
+    let h_proto = u16::from_be(unsafe { *ptr_at(&ctx, offset_of!(ethhdr, h_proto))? });
+    let ip_proto: u8 = unsafe { *ptr_at(&ctx, ETH_HDR_LEN + offset_of!(iphdr, protocol))? };
+
+    if h_proto != ETH_P_IP || ip_proto != IPPROTO_TCP {
+        return Ok(None);
+    }
+
+    let dest = u16::from_be(unsafe {
+        *ptr_at(&ctx, ETH_HDR_LEN + IP_HDR_LEN + offset_of!(tcphdr, dest))?
+    });
+
+    Ok(Some(dest))
+}
+
+#[inline(always)]
+unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+fn block_port(port: u16) -> bool {
+    unsafe { BLOCK_PORTS.get(&port).is_some() }
+}
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     unreachable!()
 }
+
+const ETH_P_IP: u16 = 0x0800;
+const IPPROTO_TCP: u8 = 6;
+const ETH_HDR_LEN: usize = mem::size_of::<ethhdr>();
+const IP_HDR_LEN: usize = mem::size_of::<iphdr>();
--- {{project-name}}/src/main.rs
+++ {{project-name}}/src/main.rs
@@ -1,9 +1,52 @@
+use std::{
+    convert::{TryFrom, TryInto},
+    process,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
+
+use aya::{
+    maps::HashMap,
+    programs::{Xdp, XdpFlags},
+    Bpf, Btf,
+};
+
 fn main() {
     if let Err(e) = try_main() {
         eprintln!("error: {:#}", e);
+        process::exit(1);
     }
 }
 
 fn try_main() -> Result<(), anyhow::Error> {
-    Ok(())
+    // load the eBPF code
+    let code = include_bytes!("../../target/bpfel-unknown-none/debug/{{ project-name }}").to_vec();
+    let mut bpf = Bpf::load(&code, Btf::from_sys_fs().ok().as_ref())?;
+
+    // insert port 8080 in the list of ports to be blocked
+    let mut map = HashMap::<_, u16, u16>::try_from(bpf.map_mut("BLOCK_PORTS")?)?;
+    map.insert(8080, 1, 0)?;
+
+    // load the XDP program
+    let prog: &mut Xdp = bpf.program_mut("xdp_fw")?.try_into()?;
+    prog.load()?;
+    prog.attach("eth0", XdpFlags::SKB_MODE)?;
+
+    // wait for SIGINT or SIGTERM
+    Ok(loop_until_terminated())
+}
+
+fn loop_until_terminated() {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting signal handler");
+
+    while running.load(Ordering::SeqCst) {}
+    println!("Exiting...");
 }
