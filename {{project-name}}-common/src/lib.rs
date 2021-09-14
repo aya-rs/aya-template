d98d90be7a52f0c0fb8144053bac58e34597c67b
Dave Tucker <dave@dtucker.co.uk>
2021-09-14 19:03:15 +0100

    Generate Skeleton Programs for all program types
    User may specify a program_type by prompt or CLI flag.
    We then generate skeleton code to the ebpf program for a noop program of
    that type.
    
    Requires cargo-generate@main
    
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- {{project-name}}-common/src/lib.rs
+++ {{project-name}}-common/src/lib.rs
@@ -1,1 +1,15 @@
 #![no_std]
+{%- if program_type == "sk_msg" %}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct SockKey {
+    pub remote_ip4: u32,
+    pub local_ip4: u32,
+    pub remote_port: u32,
+    pub local_port: u32,
+}
+
+#[cfg(feature = "userspace")]
+unsafe impl aya::Pod for SockKey {}
+{%- endif -%}
--- {{project-name}}-ebpf/src/main.rs
+++ {{project-name}}-ebpf/src/main.rs
@@ -1,5 +1,183 @@
 #![no_std]
 #![no_main]
+{% case program_type -%}
+{%- when "kprobe" %}
+use aya_bpf::{
+    macros::kprobe,
+    programs::ProbeContext,
+};
+
+#[kprobe(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: ProbeContext) -> u32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: ProbeContext) -> Result<u32, u32> {
+    Ok(0)
+}
+{%- when "kretprobe" %}
+use aya_bpf::{
+    macros::kretprobe,
+    programs::ProbeContext,
+};
+
+#[kretprobe(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: ProbeContext) -> u32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: ProbeContext) -> Result<u32, u32> {
+    Ok(0)
+}
+{%- when "uprobe" %}
+use aya_bpf::{
+    macros::uprobe,
+    programs::ProbeContext,
+};
+
+#[uprobe(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: ProbeContext) -> u32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: ProbeContext) -> Result<u32, u32> {
+    Ok(0)
+}
+{%- when "uretprobe" %}
+use aya_bpf::{
+    macros::uretprobe,
+    programs::ProbeContext,
+};
+
+#[uretprobe(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: ProbeContext) -> u32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: ProbeContext) -> Result<u32, u32> {
+    Ok(0)
+}
+{%- when "sock_ops" %}
+use aya_bpf::{
+    macros::sock_ops,
+    programs::SockOpsContext,
+};
+
+#[sock_ops(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: SockOpsContext) -> u32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: SockOpsContext) -> Result<u32, u32> {
+    Ok(0)
+}
+{%- when "sk_msg" %}
+use aya_bpf::{
+    macros::{map, sk_msg},
+    maps::SockHash,
+    programs::SkMsgContext,
+};
+use {{crate_name}}_common::SockKey;
+
+#[map(name="{{sock_map}}")]
+static mut {{sock_map}}: SockHash<SockKey> = SockHash::<SockKey>::with_max_entries(1024, 0);
+
+#[sk_msg(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: SkMsgContext) -> u32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: SkMsgContext) -> Result<u32, u32> {
+    Ok(0)
+}
+{%- when "xdp" %}
+use aya_bpf::{
+    bindings::xdp_action,
+    macros::xdp,
+    programs::XdpContext,
+};
+
+#[xdp(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: XdpContext) -> u32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: XdpContext) -> Result<u32, u32> {
+    Ok(xdp_action::XDP_PASS)
+}
+{%- when "classifier" %}
+use aya_bpf::{
+    macros::classifier,
+    programs::SkSkbContext,
+};
+
+#[classifier(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: SkSkbContext) -> i32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: SkSkbContext) -> Result<i32, i32> {
+    Ok(0)
+}
+{%- when "cgroup_skb" %}
+use aya_bpf::{
+    macros::cgroup_skb,
+    programs::SkSkbContext,
+};
+
+#[cgroup_skb(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: SkSkbContext) -> i32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: SkSkbContext) -> Result<i32, i32> {
+    Ok(0)
+}
+{%- when "tracepoint" %}
+use aya_bpf::{
+    macros::tracepoint,
+    programs::TracePointContext,
+};
+
+#[tracepoint(name="{{crate_name}}")]
+pub fn {{crate_name}}(ctx: TracePointContext) -> u32 {
+    match unsafe { try_{{crate_name}}(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_{{crate_name}}(_ctx: TracePointContext) -> Result<u32, u32> {
+    Ok(0)
+}
+{%- endcase %}
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
