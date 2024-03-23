3bee1bcc0f92e01a48d6e8ead47475d68c4ba82d
pdliyan <pdliyan@didiglobal.com>
2024-03-23 19:34:54 +0800

    Reverse of sk_msg.

--- {{project-name}}/src/main.rs
+++ {{project-name}}/src/main.rs
@@ -122,11 +122,11 @@ async fn main() -> Result<(), anyhow::Error> {
     program.load()?;
     program.attach(cgroup)?;
     {%- when "sk_msg" -%}
-    let prog: &mut SkMsg = bpf.program_mut("{{crate_name}}").unwrap().try_into()?;
-    prog.load()?;
-
     let sock_map: SockHash::<_, SockKey> = bpf.map("{{sock_map}}").unwrap().try_into()?;
     let map_fd = sock_map.fd();
+
+    let prog: &mut SkMsg = bpf.program_mut("{{crate_name}}").unwrap().try_into()?;
+    prog.load()?;
     prog.attach(map_fd)?;
     // insert sockets to the map using sock_map.insert here, or from a sock_ops program
     {%- when "xdp" -%}
