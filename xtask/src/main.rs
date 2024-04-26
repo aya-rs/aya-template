320e191ce0338b08df38ed4a4d9d942dc8a2c98a
wanjunlei <wanjunlei@kubesphere.io>
2024-04-26 10:19:04 +0800

    add a build command

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,5 @@
 mod build_ebpf;
+mod build;
 mod run;
 
 use std::process::exit;
@@ -14,6 +15,7 @@ pub struct Options {
 #[derive(Debug, Parser)]
 enum Command {
     BuildEbpf(build_ebpf::Options),
+    Build(build::Options),
     Run(run::Options),
 }
 
@@ -24,6 +26,7 @@ fn main() {
     let ret = match opts.command {
         BuildEbpf(opts) => build_ebpf::build_ebpf(opts),
         Run(opts) => run::run(opts),
+        Build(opts) => build::build(opts),
     };
 
     if let Err(e) = ret {
