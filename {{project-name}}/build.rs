f6e04be74e85a8f4a82e796cc49d8f9cac1f3f3d
Tamir Duberstein <tamird@gmail.com>
2025-10-27 08:46:55 -0400

    Add cargo_metadata build dependency
    cargo_metadata is no longer exported from aya-build.

--- {{project-name}}/build.rs
+++ {{project-name}}/build.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context as _, anyhow};
-use aya_build::{Toolchain, cargo_metadata};
+use aya_build::Toolchain;
 
 fn main() -> anyhow::Result<()> {
     let cargo_metadata::Metadata { packages, .. } = cargo_metadata::MetadataCommand::new()
@@ -10,5 +10,17 @@ fn main() -> anyhow::Result<()> {
         .into_iter()
         .find(|cargo_metadata::Package { name, .. }| name.as_str() == "{{project-name}}-ebpf")
         .ok_or_else(|| anyhow!("{{project-name}}-ebpf package not found"))?;
+    let cargo_metadata::Package {
+        name,
+        manifest_path,
+        ..
+    } = ebpf_package;
+    let ebpf_package = aya_build::Package {
+        name: name.as_str(),
+        root_dir: manifest_path
+            .parent()
+            .ok_or_else(|| anyhow!("no parent for {manifest_path}"))?
+            .as_str(),
+    };
     aya_build::build_ebpf([ebpf_package], Toolchain::default())
 }
